d3070f0da9b5dc529afa3873d5925a1804ad3611
amit singh <kumarnirajit70@gmail.com>
2025-06-23 14:00:43 +0530

    home page UI design done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -75,7 +75,7 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
-          spacing: 10,
+          spacing: 12,
           children: [
             Row(
               spacing: 15,
@@ -119,6 +119,131 @@ class _HomePageState extends State<HomePage> {
                     ),
               ),
             ),
+            Row(
+              spacing: 10,
+              children: [
+                Container(
+                  width: 100,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.filter), Text("Filters")],
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.filter), Text("Filters")],
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.filter), Text("Filters")],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("EXPLORE MORE", style: TextStyle(color: Colors.grey)),
+              ],
+            ),
+            Row(
+              spacing: 10,
+              children: [
+                Container(
+                  width: 75,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        "https://img.freepik.com/premium-vector/special-offer-banner-red-yellow-flag-shape_78946-860.jpg?semt=ais_hybrid&w=740",
+                        height: 50,
+                      ),
+                      Text("Offers"),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 75,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        "https://img.freepik.com/free-vector/3d-metal-star-isolated_1308-117760.jpg?semt=ais_hybrid&w=740",
+                        height: 50,
+                      ),
+                      Text("Top 10"),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 75,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        "https://img.freepik.com/premium-vector/realistic-train-vector-illustration-concept_1253202-29241.jpg?semt=ais_hybrid&w=740",
+                        height: 30,
+                      ),
+                      Text("Food"),
+                      Text("on train"),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 75,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        "https://www.shutterstock.com/image-vector/berger-character-logo-street-food-600nw-2622309653.jpg",
+                        height: 50,
+                      ),
+                      Text("Collections", style: TextStyle(fontSize: 12)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -126,10 +251,21 @@ class _HomePageState extends State<HomePage> {
         spacing: 30,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.home)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.home)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.home)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+          IconButton(
+            onPressed: () {},
+            icon: Row(
+              spacing: 5,
+              children: [Icon(Icons.delivery_dining), Text("Delivery")],
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Row(children: [Icon(Icons.dinner_dining), Text("Dining")]),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Row(children: [Text("district"), Icon(Icons.arrow_forward)]),
+          ),
         ],
       ),
     );
